710857fcce1cc959fdc03847aa93a8058076992c
arfan4-dev <arfan4.dev@gmail.com>
2025-05-07 10:29:13 +0500

    fix: update success toast messages for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -39,32 +39,32 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminLogin />} /> */}
         <Route path="/login" element={
-          <PublicRoute>
+          // <PublicRoute>
             <Login />
-          </PublicRoute>
+          // </PublicRoute>
         } />
 
         <Route path="/" element={
-          <PublicRoute>
+          // <PublicRoute>
             <Register />
-          </PublicRoute>
+          // </PublicRoute>
         } />
 
         <Route path="/admin" element={
-          <PublicRoute>
+          // <PublicRoute>
             <AdminLogin />
-         </PublicRoute>
+        //  </PublicRoute>
         } />
         <Route path="/forgot-password" element={
-          <PublicRoute>
+          // <PublicRoute>
           <ForgotPassword />
-          </PublicRoute>
+          // </PublicRoute>
         } />
 
         <Route path="/reset-password/:token" element={
-          <PublicRoute>
+          // <PublicRoute>
           <ResetPassword />
-          </PublicRoute>
+          // </PublicRoute>
         } />
         {/* Private Route */}
         <Route path="/service-request" element={<ProtectedRoute><ServiceRequest /></ProtectedRoute>} />
